8668a47db67d2bbb83b3d733d2a10d5a9267196f
Wolfgang Lutz <wlutz@dopanic.com>
2014-01-13 14:03:22 +0100

    Config Build and Cleanup

--- Plugins/ImageReceiver/gen/com/aware/plugin/image_receiver/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.aware.plugin.image_receiver;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Plugins/LocationDissolver/gen/com/aware/plugin/location_dissolver/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.aware.plugin.location_dissolver;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
