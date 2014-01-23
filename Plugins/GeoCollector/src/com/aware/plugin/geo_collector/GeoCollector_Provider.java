c9f3f5c60b439cbd92ce13429af1ad576c268f22
Wolfgang Lutz <wlutz@dopanic.com>
2014-01-23 11:49:53 +0100

    Added GeoCollector

---
+++ Plugins/GeoCollector/src/com/aware/plugin/geo_collector/GeoCollector_Provider.java
@@ -0,0 +1,199 @@
+package com.aware.plugin.geo_collector;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+import com.aware.Aware;
+import com.aware.utils.DatabaseHelper;
+
+import java.util.HashMap;
+
+/**
+ * ContentProvider for the Geo Collector
+ */
+
+public class GeoCollector_Provider extends ContentProvider {
+
+    private final String TAG = "GeoCollector Provider";
+    public static final String AUTHORITY = "com.aware.provider.plugin.geo_collector";
+
+    private static final int DATABASE_VERSION = 5;
+
+    private static final int GEO_COLLECTOR_TERMS = 1;
+    private static final int GEO_COLLECTOR_TERMS_ID = 2;
+
+    private static UriMatcher uriMatcher = null;
+    private static HashMap<String, String> contentMapTerms = null;
+    private static DatabaseHelper databaseHelper = null;
+    private static SQLiteDatabase database = null;
+
+    public static final class GeoCollectorTermData implements BaseColumns {
+        private GeoCollectorTermData() {
+        }
+
+        public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/plugin_geo_collector_terms"); //this needs to match the table name
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.aware.plugin.geo_collector";
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.aware.plugin.geo_collector";
+
+        public static final String _ID = "_id";
+        public static final String TIMESTAMP = "timestamp";
+        public static final String DEVICE_ID = "device_id";
+
+        /**
+         * Content of Geo
+         */
+        public static final String TERM_CONTENT = "term_content";
+
+        /**
+         * Source of Geo
+         */
+        public static final String TERM_SOURCE = "term_source";
+    }
+
+    public static String DATABASE_NAME = Environment.getExternalStorageDirectory() + "/AWARE/plugin_geo_collector.db";
+
+    public static final String[] DATABASE_TABLES = {
+            "plugin_geo_collector_terms"
+    };
+
+    public static final String[] TABLES_FIELDS = {
+            //GeoData
+            GeoCollectorTermData._ID + " integer primary key autoincrement," +
+                    GeoCollectorTermData.TIMESTAMP + " real default 0," +
+                    GeoCollectorTermData.DEVICE_ID + " text default ''," +
+                    GeoCollectorTermData.TERM_CONTENT + " text default ''," +
+                    GeoCollectorTermData.TERM_SOURCE + " text default ''",
+                    //+ "UNIQUE (" + GeoCollectorTermData.TIMESTAMP + "," + GeoCollectorTermData.DEVICE_ID + "," + GeoCollectorTermData.GEO_CONTENT + ")",
+
+
+    };
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, DATABASE_TABLES[0], GEO_COLLECTOR_TERMS);
+        uriMatcher.addURI(AUTHORITY, DATABASE_TABLES[0] + "/#", GEO_COLLECTOR_TERMS_ID);
+
+
+        contentMapTerms = new HashMap<String, String>();
+        contentMapTerms.put(GeoCollectorTermData._ID, GeoCollectorTermData._ID);
+        contentMapTerms.put(GeoCollectorTermData.TIMESTAMP, GeoCollectorTermData.TIMESTAMP);
+        contentMapTerms.put(GeoCollectorTermData.DEVICE_ID, GeoCollectorTermData.DEVICE_ID);
+        contentMapTerms.put(GeoCollectorTermData.TERM_CONTENT, GeoCollectorTermData.TERM_CONTENT);
+        contentMapTerms.put(GeoCollectorTermData.TERM_SOURCE, GeoCollectorTermData.TERM_SOURCE);
+
+
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        if (database == null || !database.isOpen()) database = databaseHelper.getWritableDatabase();
+
+        int count;
+        switch (uriMatcher.match(uri)) {
+            case GEO_COLLECTOR_TERMS:
+                count = database.delete(DATABASE_TABLES[0], selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            case GEO_COLLECTOR_TERMS:
+                return GeoCollectorTermData.CONTENT_TYPE;
+            case GEO_COLLECTOR_TERMS_ID:
+                return GeoCollectorTermData.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues initialValues) {
+        if (database == null || !database.isOpen()) database = databaseHelper.getWritableDatabase();
+
+        ContentValues values = (initialValues != null) ? new ContentValues(initialValues) : new ContentValues();
+
+        long _id;
+        switch (uriMatcher.match(uri)) {
+            case GEO_COLLECTOR_TERMS:
+                _id = database.insert(DATABASE_TABLES[0], GeoCollectorTermData.TIMESTAMP, values);
+                Log.wtf(TAG, "Id: " + _id);
+                if (_id > 0) {
+                    Uri dataUri = ContentUris.withAppendedId(GeoCollectorTermData.CONTENT_URI, _id);
+                    getContext().getContentResolver().notifyChange(dataUri, null);
+                    return dataUri;
+                }
+                throw new SQLException("Failed to insert row into " + uri);
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+    }
+
+    @Override
+    public boolean onCreate() {
+        if (databaseHelper == null)
+            databaseHelper = new DatabaseHelper(getContext(), DATABASE_NAME, null, DATABASE_VERSION, DATABASE_TABLES, TABLES_FIELDS);
+        database = databaseHelper.getWritableDatabase();
+        return (databaseHelper != null);
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        if (database == null || !database.isOpen()) database = databaseHelper.getWritableDatabase();
+
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        switch (uriMatcher.match(uri)) {
+            case GEO_COLLECTOR_TERMS:
+                qb.setTables(DATABASE_TABLES[0]);
+                qb.setProjectionMap(contentMapTerms);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        try {
+            Cursor c = qb.query(database, projection, selection, selectionArgs, null, null, sortOrder);
+            c.setNotificationUri(getContext().getContentResolver(), uri);
+            return c;
+        } catch (IllegalStateException e) {
+            if (Aware.DEBUG) Log.e(Aware.TAG, e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        if (database == null || !database.isOpen()) database = databaseHelper.getWritableDatabase();
+
+        int count;
+        switch (uriMatcher.match(uri)) {
+            case GEO_COLLECTOR_TERMS:
+                count = database.update(DATABASE_TABLES[0], values, selection, selectionArgs);
+                break;
+            default:
+                database.close();
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        Log.d(TAG, "Notifying about change");
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+}
---
+++ Plugins/GeoCollector/src/com/aware/plugin/geo_collector/Plugin.java
@@ -0,0 +1,177 @@
+package com.aware.plugin.geo_collector;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+
+import com.aware.Aware;
+import com.aware.Aware_Preferences;
+import com.aware.plugin.geo_collector.GeoCollector_Provider.GeoCollectorTermData;
+import com.aware.utils.Aware_Plugin;
+
+/**
+ * Main Plugin for the GEO Collector
+ *
+ * @author Wolfgang Lutz
+ * @email: wolfgang@lutz-wiesent.de
+ */
+
+public class Plugin extends Aware_Plugin {
+
+    private static final String TAG = "GeoCollector Plugin";
+    public static final String ACTION_AWARE_GEOCOLLECTOR = "ACTION_AWARE_GEOCOLLECTOR";
+
+    public static final String EXTRA_TERMCONTENT = "termcontent";
+    public static String lastTermContent = "";
+
+    public static Uri geonameResolverContentUri;
+    private static GeonameResolverObserver geonameResolverObs = null;
+
+    public static Uri termCollectorGeoTermContentUri;
+    private static TermCollectorGeoTermObserver termCollectorGeoTermObs = null;
+
+    /**
+     * Thread manager
+     */
+    private static HandlerThread threads = null;
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "Plugin Created");
+        super.onCreate();
+
+
+        // Share the context back to the framework and other applications
+        CONTEXT_PRODUCER = new Aware_Plugin.ContextProducer() {
+            @Override
+            public void onContext() {
+                Log.d(TAG, "Putting extra context into intent");
+                Intent notification = new Intent(ACTION_AWARE_GEOCOLLECTOR);
+                notification
+                        .putExtra(Plugin.EXTRA_TERMCONTENT, lastTermContent);
+                sendBroadcast(notification);
+            }
+        };
+
+        DATABASE_TABLES = GeoCollector_Provider.DATABASE_TABLES;
+        TABLES_FIELDS = GeoCollector_Provider.TABLES_FIELDS;
+        CONTEXT_URIS = new Uri[]{GeoCollectorTermData.CONTENT_URI};
+
+        threads = new HandlerThread(TAG);
+        threads.start();
+
+        // Set the observers, that run in independent threads, for
+        // responsiveness
+
+
+        geonameResolverContentUri = Uri
+                .parse("content://com.aware.provider.plugin.geoname_resolver/plugin_geoname_resolver");
+        geonameResolverObs = new GeonameResolverObserver(new Handler(
+                threads.getLooper()));
+        getContentResolver().registerContentObserver(
+                geonameResolverContentUri, true, geonameResolverObs);
+        Log.d(TAG, "geonameResolverObs registered");
+
+        termCollectorGeoTermContentUri = Uri
+                .parse("content://com.aware.provider.plugin.term_collector/plugin_term_collector_geodata");
+        termCollectorGeoTermObs = new TermCollectorGeoTermObserver(new Handler(
+                threads.getLooper()));
+        getContentResolver().registerContentObserver(
+                termCollectorGeoTermContentUri, true, termCollectorGeoTermObs);
+        Log.d(TAG, "termCollectorGeoTermObs registered");
+
+
+        Log.d(TAG, "Plugin Started");
+    }
+
+    @Override
+    public void onDestroy() {
+
+        Log.d(TAG, "Plugin is destroyed");
+
+        super.onDestroy();
+
+        getContentResolver().unregisterContentObserver(geonameResolverObs);
+        getContentResolver().unregisterContentObserver(termCollectorGeoTermObs);
+
+    }
+
+
+    public class TermCollectorGeoTermObserver extends ContentObserver {
+        public TermCollectorGeoTermObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+
+            Log.wtf(TAG, "@TermCollectorGeoTermObserver");
+
+            // set cursor to first item
+            Cursor cursor = getContentResolver().query(
+                    termCollectorGeoTermContentUri, null, null, null,
+                    "timestamp" + " DESC LIMIT 1");
+            if (cursor != null && cursor.moveToFirst()) {
+
+                saveGeoData(cursor.getLong(cursor.getColumnIndex("timestamp")),
+                        termCollectorGeoTermContentUri.toString(), cursor.getString(cursor
+                        .getColumnIndex("term_content")));
+            }
+
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
+
+    }
+
+    public class GeonameResolverObserver extends ContentObserver {
+        public GeonameResolverObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+
+            Log.wtf(TAG, "@GeonameResolverObs");
+
+            // set cursor to first item
+            Cursor cursor = getContentResolver().query(
+                    geonameResolverContentUri, null, null, null,
+                    "timestamp" + " DESC LIMIT 1");
+            if (cursor != null && cursor.moveToFirst()) {
+
+                saveGeoData(cursor.getLong(cursor.getColumnIndex("timestamp")),
+                        geonameResolverContentUri.toString(), cursor.getString(cursor
+                        .getColumnIndex("NAME")));
+            }
+
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
+
+    }
+
+    private void saveGeoData(long timestamp, String source, String content) {
+        Log.d(TAG, "Saving Data");
+
+        ContentValues rowData = new ContentValues();
+        rowData.put(GeoCollectorTermData.DEVICE_ID, Aware.getSetting(
+                getContentResolver(), Aware_Preferences.DEVICE_ID));
+        rowData.put(GeoCollectorTermData.TIMESTAMP, timestamp);
+        rowData.put(GeoCollectorTermData.TERM_SOURCE, source);
+        rowData.put(GeoCollectorTermData.TERM_CONTENT, content);
+
+        Log.d(TAG, "Saving " + rowData.toString());
+        getContentResolver().insert(GeoCollectorTermData.CONTENT_URI, rowData);
+    }
+
+}
