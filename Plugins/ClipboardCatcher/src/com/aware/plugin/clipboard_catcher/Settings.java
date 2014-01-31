3ed05034a42d55ea5ead254454c7aba24b1fc53d
Wolfgang Lutz <wlutz@dopanic.com>
2014-01-31 17:49:35 +0100

    Added Clean Data Button

--- Plugins/ClipboardCatcher/src/com/aware/plugin/clipboard_catcher/Settings.java
+++ Plugins/ClipboardCatcher/src/com/aware/plugin/clipboard_catcher/Settings.java
@@ -1,37 +1,23 @@
 package com.aware.plugin.clipboard_catcher;
 
-import com.aware.plugin.clipboard_catcher.ClipboardCatcher_Provider.ClipboardCatcher;
-
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class Settings extends Activity {
     private final static String TAG = "ClipboardCatcher Settings";
-    private static TextView content = null;
     private static TextView countView = null;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        IntentFilter clipboard_filter = new IntentFilter();
-        clipboard_filter.addAction(Plugin.ACTION_AWARE_CLIPBOARDCATCHER);
-        
-        Log.d(TAG,"Creating Receiver");
-        registerReceiver(clipboard_receiver, clipboard_filter);
         
         setContentView(R.layout.debug_layout);
         
         countView = (TextView) findViewById(R.id.count);
-        
-        content = (TextView) findViewById(R.id.clipboardcontent);        
-        content.setText(Plugin.currentClipboardContent);
         
         updateCount();
     }
@@ -41,27 +27,9 @@ public class Settings extends Activity {
     	updateCount();
     }
     
-    
-    private static final ClipboardReceiver clipboard_receiver = new ClipboardReceiver();
-    
-    public static class ClipboardReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-			Log.d(TAG,"received");
-
-        	if( intent.getAction().equals(Plugin.ACTION_AWARE_CLIPBOARDCATCHER)) {
-        		Log.d(TAG,"Action");
-        		content.setText(intent.getStringExtra(Plugin.EXTRA_CLIPBOARDCONTENT));
-            }
-        }
-    }
-    
     @Override
     protected void onDestroy() {
-    	Log.d(TAG,"Destroying Receiver");
     	super.onDestroy();
-        
-        unregisterReceiver(clipboard_receiver);
     }
     
     public void updateCount() {
@@ -73,9 +41,14 @@ public class Settings extends Activity {
 
         countCursor.moveToFirst();
         int count = countCursor.getInt(0);
-        
-        Log.d(TAG, "count:" + count);
-        TextView myCountView = (TextView) findViewById(R.id.count);
-        myCountView.setText("" + count);
+
+        countView.setText("" + count);
+    }
+
+    public void cleanData(View view){
+        Log.d(TAG, "Trying to delete all Data.");
+        getContentResolver().delete(ClipboardCatcher_Provider.ClipboardCatcher.CONTENT_URI, " 1 = 1 ", null);
+        Log.d(TAG, "Deletion done.");
+        updateCount();
     }
 }
