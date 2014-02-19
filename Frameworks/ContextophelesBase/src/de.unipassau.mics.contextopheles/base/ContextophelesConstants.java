c24588c66b0e51317fc4e516340acc9468a89888
Wolfgang Lutz <wlutz@dopanic.com>
2014-02-19 17:20:38 +0100

    Added BaseFramework to manage common Utils and Constants

---
+++ Frameworks/ContextophelesBase/src/de.unipassau.mics.contextopheles/base/ContextophelesConstants.java
@@ -0,0 +1,144 @@
+package de.unipassau.mics.contextopheles.base;
+
+import android.net.Uri;
+
+public class ContextophelesConstants {
+    // Tags
+    public static final String TAG_AUTOMATIC_QUERY = "Contextopheles AutomaticQuery";
+    public static final String TAG_CLIPBOARD_CATCHER = "Contextopheles Clipboard Catcher";
+    public static final String TAG_GEONAME_RESOLVER = "GeonameResolver";
+    public static final String TAG_OSMPOI_RESOLVER = "OSMPoiResolver";
+
+    // Settings Fields, AQ = Automatic Query, GR = GeonameResolver, OR = OSMPOIResolver
+    public static final String SETTINGS_AQ_END_OF_DND = "AQ_END_OF_DND";
+    public static final String SETTINGS_AQ_USE_LOCATION = "AQ_USE_LOCATION";
+    public static final String SETTINGS_AQ_NOTIFICATION_VIBRATE = "AQ_QUERY_NOTIFICATION_VIBRATE";
+    public static final String SETTINGS_AQ_NOTIFICATION_SOUND = "AQ_QUERY_NOTIFICATION_SOUND";
+    public static final String SETTINGS_AQ_MINIMUM_NUMBER_OF_RESULTS_TO_DISPLAY_NOTIFICATION= "AQ_MINIMUM_NUMBER_OF_RESULTS_TO_DISPLAY_NOTIFICATION";
+    public static final String SETTINGS_GR_DISTANCE = "SETTINGS_GR_DISTANCE";
+    public static final String SETTINGS_OR_DISTANCE = "SETTINGS_OR_DISTANCE";
+    public static final String SETTINGS_USE_FAKE_LOCATION = "SETTINGS_USE_FAKE_LOCATION";
+    public static final String SETTINGS_FAKE_LONGITUDE = "SETTINGS_FAKE_LONGITUDE";
+    public static final String SETTINGS_FAKE_LATITUDE = "SETTINGS_FAKE_LATITUDE";
+    public static final String GEONAME_DISTANCE_SEEKBAR_PROGRESS = "GEONAME_DISTANCE_SEEKBAR_PROGRESS";
+    public static final String OSMPOI_DISTANCE_SEEKBAR_PROGRESS = "OSMPOI_DISTANCE_SEEKBAR_PROGRESS";
+
+    // Default Values
+    public static final boolean SETTINGS_AQ_NOTIFICATION_VIBRATE_DEFAULT = true;
+    public static final boolean SETTINGS_AQ_NOTIFICATION_SOUND_DEFAULT = true;
+    public static final boolean SETTINGS_AQ_USE_LOCATION_DEFAULT = true;
+    public static final int     SETTINGS_AQ_MINIMUM_NUMBER_OF_RESULTS_TO_DISPLAY_NOTIFICATION_DEFAULT = 5;
+    public static final long    INFO_AQ_LAST_SUCCESSFUL_QUERY_DEFAULT = 0;
+    public static final long    SETTINGS_AQ_END_OF_DND_DEFAULT = 0;
+    public static final float   SETTINGS_GR_DISTANCE_DEFAULT = 1f;
+    public static final float   SETTINGS_OR_DISTANCE_DEFAULT = 1f;
+    public static final boolean SETTINGS_USE_FAKE_LOCATION_DEFAULT = true;
+    public static final double  SETTINGS_FAKE_LATITUDE_DEFAULT = 0.0;
+    public static final double  SETTINGS_FAKE_LONGITUDE_DEFAULT = 0.0;
+    public static final int     GEONAME_DISTANCE_SEEKBAR_PROGRESS_DEFAULT = 1;
+    public static final int     OSMPOI_DISTANCE_SEEKBAR_PROGRESS_DEFAULT = 1;
+
+    // Fields for Values Saved via Settings
+    public static final String INFO_AQ_LAST_SUCCESSFUL_QUERY = "AWARE_LAST_SUCCESSFUL_QUERY";
+    public static final String INFO_AQ_LAST_TIME_USER_CLICKED_RESULTITEM = "AWARE_LAST_TIME_USER_CLICKED_RESULTITEM";
+
+    // Automatic Query Defaults
+    public static final long AQ_PLUGIN_TIME_TO_WAIT_BETWEEN_RUNS = 1000;
+    public static final long AQ_PLUGIN_MAX_NUMBER_OF_EMPTY_RUNS_BEFORE_SLEEP = 150;
+    public static final int AQ_PLUGIN_MAX_NUMBER_OF_NOTIFICATIONS_AT_ONCE = 5;
+
+    //Clipboard Catcher
+    public static final String CLIPBOARD_CATCHER_PLUGIN_NAME = "plugin.clipboard_catcher";
+    public static final String CLIPBOARD_CATCHER_AUTHORITY = "com.aware.provider." + CLIPBOARD_CATCHER_PLUGIN_NAME;
+    public static final String CLIPBOARD_CATCHER_MAIN_TABLE = "plugin_clipboard_catcher";
+
+    public static final Uri    CLIPBOARD_CATCHER_CONTENT_URI = Uri.parse("content://"+CLIPBOARD_CATCHER_AUTHORITY+"/"+ CLIPBOARD_CATCHER_MAIN_TABLE); //this needs to match the table name
+    public static final String CLIPBOARD_CATCHER_CONTENT_TYPE = "vnd.android.cursor.dir/vnd.aware."+CLIPBOARD_CATCHER_PLUGIN_NAME;
+    public static final String CLIPBOARD_CATCHER_CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.aware."+CLIPBOARD_CATCHER_PLUGIN_NAME;
+
+    public static final String CLIPBOARD_CATCHER_FIELD_ID = "_id";
+    public static final String CLIPBOARD_CATCHER_FIELD_TIMESTAMP = "timestamp";
+    public static final String CLIPBOARD_CATCHER_FIELD_DEVICE_ID = "device_id";
+    public static final String CLIPBOARD_CATCHER_FIELD_CLIPBOARDCONTENT = "CLIPBOARDCONTENT";
+
+    public static final int    CLIPBOARD_CATCHER_MAX_STORAGE = 5;
+    public static final long   CLIPBOARD_CATCHER_WEAROFF_TIME = 5 * 60 * 1000;
+
+
+    // Geoname Resolver
+    public static final String GEONAME_RESOLVER_PLUGIN_NAME= "plugin.geoname_resolver";
+    public static final String GEONAME_RESOLVER_AUTHORITY = "com.aware.provider."+GEONAME_RESOLVER_PLUGIN_NAME;
+    public static final String GEONAME_RESOLVER_MAIN_TABLE = "plugin_geoname_resolver";
+
+    public static final Uri    GEONAME_RESOLVER_CONTENT_URI = Uri.parse("content://"+GEONAME_RESOLVER_AUTHORITY+"/"+GEONAME_RESOLVER_MAIN_TABLE); //this needs to match the table name
+    public static final String GEONAME_RESOLVER_CONTENT_TYPE = "vnd.android.cursor.dir/vnd.aware."+GEONAME_RESOLVER_PLUGIN_NAME;
+    public static final String GEONAME_RESOLVER_CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.aware."+GEONAME_RESOLVER_PLUGIN_NAME;
+
+    public static final String GEONAME_RESOLVER_FIELD_ID = "_id";
+    public static final String GEONAME_RESOLVER_FIELD_TIMESTAMP = "timestamp";
+    public static final String GEONAME_RESOLVER_FIELD_DEVICE_ID = "device_id";
+    public static final String GEONAME_RESOLVER_FIELD_NAME = "name";
+
+    public static final int    GEONAME_RESOLVER_MAX_STORAGE = 7;
+    public static final long   GEONAME_RESOLVER_WEAROFF_TIME = 15 * 60 * 1000;
+
+    // OSM Poi Resolver
+
+    public static final String OSMPOI_RESOLVER_PLUGIN_NAME = "plugin.osmpoi_resolver";
+    public static final String OSMPOI_RESOLVER_AUTHORITY = "com.aware.provider."+ OSMPOI_RESOLVER_PLUGIN_NAME;
+    public static final String OSMPOI_RESOLVER_MAIN_TABLE = "plugin_osmpoi_resolver";
+
+    public static final Uri     OSMPOI_RESOLVER_CONTENT_URI = Uri.parse("content://"+OSMPOI_RESOLVER_AUTHORITY+"/"+OSMPOI_RESOLVER_MAIN_TABLE); //this needs to match the table name
+    public static final String  OSMPOI_RESOLVER_CONTENT_TYPE = "vnd.android.cursor.dir/vnd.aware."+OSMPOI_RESOLVER_PLUGIN_NAME;
+    public static final String  OSMPOI_RESOLVER_CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.aware."+OSMPOI_RESOLVER_PLUGIN_NAME;
+
+    public static final String  OSMPOI_RESOLVER_FIELD_ID = "_id";
+    public static final String  OSMPOI_RESOLVER_FIELD_TIMESTAMP = "timestamp";
+    public static final String  OSMPOI_RESOLVER_FIELD_DEVICE_ID = "device_id";
+    public static final String  OSMPOI_RESOLVER_FIELD_NAME = "name";
+    public static final String  OSMPOI_RESOLVER_FIELD_TYPE = "type";
+
+
+    //Term Collector Term
+    public static final String TERM_COLLECTOR_TERM_URI = "content://com.aware.provider.plugin.term_collector/plugin_term_collector_terms";
+    public static final String TERM_COLLECTOR_TERM_FIELD_TIMESTAMP = "timestamp";
+    public static final String TERM_COLLECTOR_TERM_FIELD_TERM_CONTENT = "term_content";
+    public static final String TERM_COLLECTOR_TERM_FIELD_TERM_SOURCE = "term_source";
+
+    //Geo Collector Term
+    public static final String GEO_COLLECTOR_TERM_URI = "content://com.aware.provider.plugin.geo_collector/plugin_geo_collector_terms";
+    public static final String GEO_COLLECTOR_TERM_FIELD_TIMESTAMP = "timestamp";
+    public static final String GEO_COLLECTOR_TERM_FIELD_TERM_CONTENT = "term_content";
+    public static final String GEO_COLLECTOR_TERM_FIELD_TERM_SOURCE = "term_source";
+
+    // Light Sensor
+    public static final String LIGHT_URI = "content://com.aware.provider.light/light";
+    public static final String LIGHT_FIELD_TIMESTAMP = "timestamp";
+    public static final String LIGHT_FIELD_DOUBLE_LUX = "double_light_lux";
+
+    // Location
+
+    public static final String LOCATION_URI = "content://com.aware.provider.locations/locations";
+
+    //SituationManager Fields
+    public static final String  SITUATION_MANAGER_LIGHT = "light";
+    public static final long    SITUATION_MANAGER_MINIMU_TIME_BETWEEN_QUERIES = 15000;
+
+
+    public static final String UI_CONTENT_URI = "content://com.aware.provider.plugin.ui_content/plugin_ui_content";
+    public static final int    UI_CONTENT_MAX_STORAGE = 5;
+    public static final long   UI_CONTENT_WEAROFF_TIME = 5 * 60 * 1000;
+    public static final String OSMPOI_RESOLVER_URI = "content://com.aware.provider.plugin.osmpoi_resolver/plugin_osmpoi_resolver";
+    public static final int    OSMPOI_RESOLVER_MAX_STORAGE = 5;
+    public static final long   OSMPOI_RESOLVER_WEAROFF_TIME = 5 * 60 * 1000;
+    public static final String NOTIFICATION_CATCHER_URI = "content://com.aware.provider.plugin.notification_catcher/plugin_notification_catcher";
+    public static final int    NOTIFICATION_CATCHER_MAX_STORAGE = 5;
+    public static final long   NOTIFICATION_CATCHER_WEAROFF_TIME = 5 * 60 * 1000;
+    public static final String SMS_RECEIVER_URI = "content://com.aware.provider.plugin.sms_receiver/plugin_sms_receiver";
+    public static final int    SMS_RECEIVER_MAX_STORAGE = 5;
+    public static final long   SMS_RECEIVER_WEAROFF_TIME = 5 * 60 * 1000;
+    public static final String TERM_COLLECTOR_GEODATA_URI = "content://com.aware.provider.plugin.term_collector/plugin_term_collector_geodata";
+    public static final int    TERM_COLLECTOR_GEODATA_MAX_STORAGE = 7;
+    public static final long   TERM_COLLECTOR_GEODATA_WEAROFF_TIME = 5 * 60 * 1000;
+
+}
---
+++ Frameworks/ContextophelesBase/src/de.unipassau.mics.contextopheles/utils/CommonSettings.java
@@ -0,0 +1,243 @@
+package de.unipassau.mics.contextopheles.utils;
+
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import com.aware.Aware;
+
+import de.unipassau.mics.contextopheles.base.ContextophelesConstants;
+
+public class CommonSettings {
+    private static final String TAG = "CommonSettings";
+
+    private static boolean getBooleanFromAwareSettings(ContentResolver resolver,  String settingsIdentifier, boolean defaultValue) {
+        String booleanString = Aware.getSetting(resolver, settingsIdentifier);
+        if (booleanString != null) {
+            try {
+                return Boolean.parseBoolean(booleanString);
+            } catch (NumberFormatException e) {
+                return defaultValue;
+            }
+        } else {
+            return defaultValue;
+        }
+    }
+
+
+    private static int getIntegerFromAwareSettings(ContentResolver resolver,  String settingsIdentifier, int defaultValue) {
+        String integerString = Aware.getSetting(resolver, settingsIdentifier);
+        if (integerString != null) {
+            try {
+                return Integer.parseInt(integerString);
+            } catch (NumberFormatException e) {
+                return defaultValue;
+            }
+        } else {
+            return defaultValue;
+        }
+    }
+
+    private static long getLongFromAwareSettings(ContentResolver resolver,  String settingsIdentifier, long defaultValue) {
+        String longString = Aware.getSetting(resolver, settingsIdentifier);
+        if (longString != null) {
+            try {
+                return Long.parseLong(longString);
+            } catch (NumberFormatException e) {
+                return defaultValue;
+            }
+        } else {
+            return defaultValue;
+        }
+    }
+
+    private static float getFloatFromAwareSettings(ContentResolver resolver,  String settingsIdentifier, float defaultValue) {
+        String floatString = Aware.getSetting(resolver, settingsIdentifier);
+        if (floatString  != null) {
+            try {
+                return Float.parseFloat(floatString);
+            } catch (NumberFormatException e) {
+                return defaultValue;
+            }
+        } else {
+            return defaultValue;
+        }
+    }
+
+    private static double getDoubleFromAwareSettings(ContentResolver resolver,  String settingsIdentifier, double defaultValue) {
+        String doubleString = Aware.getSetting(resolver, settingsIdentifier);
+        if (doubleString != null) {
+            try {
+                return Double.parseDouble(doubleString);
+            } catch (NumberFormatException e) {
+                return defaultValue;
+            }
+        } else {
+            return defaultValue;
+        }
+    }
+
+    public static boolean getNotificationUsesVibration(ContentResolver resolver) {
+        boolean result = getBooleanFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_AQ_NOTIFICATION_VIBRATE, ContextophelesConstants.SETTINGS_AQ_NOTIFICATION_VIBRATE_DEFAULT);
+        //Log.d(TAG, "@getNotificationUsesVibration " + result);
+        return result;
+    }
+
+    public static void setNotificationUsesVibration(ContentResolver resolver, Boolean value) {
+        //Log.d(TAG, "@setNotificationUsesVibration: "  + value );
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_AQ_NOTIFICATION_VIBRATE, value.toString());
+    }
+
+    public static boolean getNotificationUsesSound(ContentResolver resolver) {
+        boolean result = getBooleanFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_AQ_NOTIFICATION_SOUND, ContextophelesConstants.SETTINGS_AQ_NOTIFICATION_SOUND_DEFAULT);
+        //Log.d(TAG, "@getNotificationUsesSound " + result);
+        return result;
+    }
+
+    public static void setNotificationUsesSound(ContentResolver resolver, Boolean value) {
+        //Log.d(TAG, "@setNotificationUsesSound: " + value );
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_AQ_NOTIFICATION_SOUND, value.toString());
+    }
+
+    public static int getMinimumNumberOfResultsToDisplayNotification(ContentResolver resolver) {
+        return getIntegerFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_AQ_MINIMUM_NUMBER_OF_RESULTS_TO_DISPLAY_NOTIFICATION, ContextophelesConstants.SETTINGS_AQ_MINIMUM_NUMBER_OF_RESULTS_TO_DISPLAY_NOTIFICATION_DEFAULT);
+    }
+
+    public static void setMinimumNumberOfResultsToDisplayNotification(ContentResolver resolver, Integer value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_AQ_MINIMUM_NUMBER_OF_RESULTS_TO_DISPLAY_NOTIFICATION, value.toString());
+    }
+
+
+    public static long getTimeOfLastSuccessfulQuery(ContentResolver resolver) {
+        return getLongFromAwareSettings(resolver, ContextophelesConstants.INFO_AQ_LAST_SUCCESSFUL_QUERY, ContextophelesConstants.INFO_AQ_LAST_SUCCESSFUL_QUERY_DEFAULT);
+    }
+
+    public static void setTimeOfLastSuccessfulQuery(ContentResolver resolver, Long value) {
+        Aware.setSetting(resolver, ContextophelesConstants.INFO_AQ_LAST_SUCCESSFUL_QUERY, value.toString());
+    }
+
+
+    public static long getLastTimeUserClickedResultItem(ContentResolver resolver) {
+        return getLongFromAwareSettings(resolver, ContextophelesConstants.INFO_AQ_LAST_TIME_USER_CLICKED_RESULTITEM, ContextophelesConstants.INFO_AQ_LAST_SUCCESSFUL_QUERY_DEFAULT);
+    }
+
+    public static void setLastTimeUserClickedResultItem(ContentResolver resolver, Long value) {
+        Aware.setSetting(resolver, ContextophelesConstants.INFO_AQ_LAST_TIME_USER_CLICKED_RESULTITEM, value.toString());
+    }
+
+
+    public static long getEndOfDoNotDisturb(ContentResolver resolver) {
+        return getLongFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_AQ_END_OF_DND, ContextophelesConstants.SETTINGS_AQ_END_OF_DND_DEFAULT);
+    }
+
+    public static void setEndOfDoNotDisturb(ContentResolver resolver, Long value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_AQ_END_OF_DND, value.toString());
+    }
+
+    public static boolean getQueryUseOfLocation(ContentResolver resolver) {
+        return getBooleanFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_AQ_USE_LOCATION, ContextophelesConstants.SETTINGS_AQ_USE_LOCATION_DEFAULT);
+    }
+
+    public static void setQueryUseOfLocation(ContentResolver resolver, Boolean value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_AQ_USE_LOCATION, value.toString());
+    }
+
+    public static float getGeonameDistance(ContentResolver resolver) {
+        return getFloatFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_GR_DISTANCE, ContextophelesConstants.SETTINGS_GR_DISTANCE_DEFAULT);
+    }
+
+    public static void setGeonameDistance(ContentResolver resolver, Float value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_GR_DISTANCE, value.toString());
+    }
+
+    public static void setGeonameDistanceFromString(ContentResolver resolver, String value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_GR_DISTANCE, value);
+    }
+
+    public static int getGeonameDistanceSeekBarProgress(ContentResolver resolver) {
+        return getIntegerFromAwareSettings(resolver, ContextophelesConstants.GEONAME_DISTANCE_SEEKBAR_PROGRESS, ContextophelesConstants.GEONAME_DISTANCE_SEEKBAR_PROGRESS_DEFAULT);
+    }
+
+    public static void setGeonameDistanceSeekBarProgress(ContentResolver resolver, Integer value) {
+        Aware.setSetting(resolver, ContextophelesConstants.GEONAME_DISTANCE_SEEKBAR_PROGRESS, value.toString());
+    }
+
+    public static float getOSMPoiDistance(ContentResolver resolver) {
+        return getFloatFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_OR_DISTANCE, ContextophelesConstants.SETTINGS_OR_DISTANCE_DEFAULT);
+    }
+
+    public static void setOSMPoiDistance(ContentResolver resolver, Float value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_OR_DISTANCE, value.toString());
+    }
+
+    public static void setOSMPoiDistanceFromString(ContentResolver resolver, String value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_OR_DISTANCE, value);
+    }
+
+    public static int getOSMPoiDistanceSeekBarProgress(ContentResolver resolver) {
+        return getIntegerFromAwareSettings(resolver, ContextophelesConstants.OSMPOI_DISTANCE_SEEKBAR_PROGRESS, ContextophelesConstants.OSMPOI_DISTANCE_SEEKBAR_PROGRESS_DEFAULT);
+    }
+
+    public static void setOSMPoiDistanceSeekBarProgress(ContentResolver resolver, Integer value) {
+        Aware.setSetting(resolver, ContextophelesConstants.OSMPOI_DISTANCE_SEEKBAR_PROGRESS, value.toString());
+    }
+
+    public static boolean getUseFakeLocation(ContentResolver resolver) {
+        return getBooleanFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_USE_FAKE_LOCATION, ContextophelesConstants.SETTINGS_USE_FAKE_LOCATION_DEFAULT);
+    }
+
+    public static void setUseFakeLocation(ContentResolver resolver, Boolean value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_USE_FAKE_LOCATION, value.toString());
+    }
+
+    public static void setFakeLongitude(ContentResolver resolver, Double value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_FAKE_LONGITUDE, value.toString());
+    }
+
+
+    public static void setFakeLongitudeFromString(ContentResolver resolver, String value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_FAKE_LONGITUDE, value);
+    }
+
+    public static double getFakeLongitude(ContentResolver resolver) {
+        return getDoubleFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_FAKE_LONGITUDE, ContextophelesConstants.SETTINGS_FAKE_LONGITUDE_DEFAULT);
+    }
+
+    public static void setFakeLatitude(ContentResolver resolver, Double value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_FAKE_LATITUDE, value.toString());
+    }
+
+    public static void setFakeLatitudeFromString(ContentResolver resolver, String value) {
+        Aware.setSetting(resolver, ContextophelesConstants.SETTINGS_FAKE_LATITUDE, value);
+    }
+
+    public static double getFakeLatitude(ContentResolver resolver) {
+        return getDoubleFromAwareSettings(resolver, ContextophelesConstants.SETTINGS_FAKE_LATITUDE, ContextophelesConstants.SETTINGS_FAKE_LATITUDE_DEFAULT);
+    }
+
+
+
+
+
+
+
+    public static int getCountForUri(ContentResolver resolver, Uri uri) {
+        Cursor countCursor = resolver.query(uri,
+                new String[] {"count(*) AS count"},
+                null,
+                null,
+                null);
+
+        countCursor.moveToFirst();
+        return countCursor.getInt(0);
+    }
+
+    public static void cleanDataForUri(ContentResolver resolver, Uri uri){
+        Log.d(TAG, "Trying to delete all Data.");
+        resolver.delete(uri, " 1 = 1 ", null);
+        Log.d(TAG, "Deletion done.");
+    }
+
+}
